a3abe6ff51ece209f4d9e0ee592a8f59314d3329
damenjoSitepu <damenstory@gmail.com>
2022-07-20 11:12:45 +0700

    Adding Excel Import Page View

---
+++ resources/views/template/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tutorial</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+<body>
+    {{-- Submenu --}}
+    @include('template.submenu.index')
+    {{-- End Submenu --}}
+
+    {{-- Content --}}
+    <div class="container">
+        @yield('content')
+    </div>
+    {{-- End Content --}}
+
+    <!-- Bottom Source Script -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/fbc67db110.js" crossorigin="anonymous"></script>
+    <script src="{{ asset('assets/js/logic.js') }}"></script>
+    <script>
+</body>
+</html>
+
--- resources/views/tutorial/index.blade.php
+++ resources/views/tutorial/index.blade.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tutorial</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-</head>
-<body>
-    <h1 class="btn btn-primary">Hello World</h1>
-</body>
-</html>
+@extends('template.index')
+
+@section('content')
+<div class="mt-5">
+    <div class="mb-3 w-50 m-auto text-center">
+        <label for="formFile" class="form-label fw-bold"><i class="fas fa-file-excel text-success"></i> Pilih File Import Excel</label>
+        <input class="form-control" type="file" id="formFile">
+    </div>
+</div>
+@endSection()
